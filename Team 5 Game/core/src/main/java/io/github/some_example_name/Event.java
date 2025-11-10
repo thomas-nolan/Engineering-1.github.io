f51e6cb9538183b85de93b8aa5bfa38a39c26e50
thomas-nolan <t.nolan2592@gmail.com>
2025-11-10 00:22:02 +0000

    Change Event to an interface

--- Team 5 Game/core/src/main/java/io/github/some_example_name/Event.java
+++ Team 5 Game/core/src/main/java/io/github/some_example_name/Event.java
@@ -2,48 +2,16 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.math.Rectangle;
 
-/* This abstract class manages the multiple events that appear in the game
+/* This interface manages the multiple events that appear in the game
      * 
      * Each event is named and has a location.
      * 
 */
-public abstract class Event {
-    private String name;
-    private boolean isTriggered;
+public interface Event {
     
-    private static int eventsCounter; 
-    
-    public Event(String name) {
-        this.name = name;
-        this.isTriggered = false;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    protected void incrementEventsCounter () {
-        eventsCounter++;
-        System.out.println(eventsCounter);
-    }
-
-    public static void resetEventsCounter () {
-        eventsCounter = 0;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isTriggered() {
-        return isTriggered;
-    }
-
-    public void setTriggered(boolean triggered) {
-        this.isTriggered = triggered;
-    }
-
-
-    public abstract void trigger();
+    String getName(); 
+    void setName(String name);
+    boolean isTriggered();
+    void setTriggered(boolean triggered);
+    void trigger();
 }
